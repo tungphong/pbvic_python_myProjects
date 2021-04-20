e36ad2fae004afbe9201a0dd4f54c2b48dd21f54
Vinh Bui <vinhphucvn@yahoo.com>
2021-04-20 13:08:17 -0700

    pbvic - check files and folder.

---
+++ pbvic_Folder&file_proj/collect_files.py
@@ -0,0 +1,84 @@
+import os
+import sys
+
+import stat
+import time
+import datetime
+
+### Funs
+def windows_print_folder(inputpath, files_folders):
+    open_file = open('C:\\tungphong\\pbvic_python\\pbvic_work_proj\\folder_file\\test_folder\\test.txt', 'w')
+    open_file.write("####################################################\n")
+    open_file.write("#       Directory List For <directory_name>        #\n")
+    open_file.write("####################################################\n")
+
+    for fileOrfolder in files_folders:
+        if os.path.isfile(inputpath+"\\"+str(fileOrfolder)):
+            open_file.write(f"File name: {fileOrfolder}\n")
+        else:
+            open_file.write(f"<DIR>   {fileOrfolder}\n")
+        fileStatsObj = os.stat(inputpath+"\\"+str(fileOrfolder))
+        modificationTime = time.ctime(fileStatsObj[stat.ST_MTIME])
+        open_file.write(f"    Last Modified Time :  {modificationTime}\n")
+        open_file.write("----------------------------------------------\n")
+    open_file.close()
+def MacNLinux_print_folder(inputpath, files_folders):
+    for fileOrfolder in files_folders:
+        if os.path.isfile(inputpath+"\\"+str(fileOrfolder)):
+            print(f"File name: {fileOrfolder}\n")
+        else:
+            print(f"<DIR>   {fileOrfolder}\n")
+        fileStatsObj = os.stat(inputpath+"\\"+str(fileOrfolder))
+        modificationTime = time.ctime(fileStatsObj[stat.ST_MTIME])
+        print(f"    Last Modified Time :  {modificationTime}\n")
+        print("----------------------------------------------\n")
+
+
+def no_of_argu(*args):
+    # using len() method in args to count
+    return (len(args))
+###---------------------------------------------------
+
+###=============main func=====================
+def main():
+    # Count the arguments
+    arguments = len(sys.argv) - 1
+    print("Number argument: ", arguments)
+
+    inputpath = sys.argv[arguments]
+    ## Check the folder exist or not, and then printout
+    if os.path.exists(inputpath):
+        print(inputpath)
+
+    ## Check OS system
+    os_check = sys.platform
+    #print(os_check)
+
+    #inputpath = input("Please enter your folder with full path: \n")
+    #print(f'Your enter: {inputpath}')
+    #files_folders = os.listdir(inputpath)
+    #print(files_folders)
+
+
+
+    if os_check == 'linux' or os_check == 'linux2':
+        ## Check files in Linux system
+        print(os_check)
+        MacNLinux_print_folder(inputpath, files_folders)
+    elif os_check == "darwin":
+        ## Check files in Mac system
+        print(os_check)
+        MacNLinux_print_folder(inputpath, files_folders)
+    elif os_check == "win32":
+        ## Check files in widow system
+        print(os_check)
+        #windows_print_folder(inputpath, files_folders)
+    elif os_check == "win64":
+        ## Check files in widow system
+        print(os_check)
+        windows_print_folder(inputpath, files_folders)
+
+if __name__=="__main__":
+    main()
+else:
+    exit()
---
+++ pbvic_Folder&file_proj/test_file_1.py
@@ -0,0 +1,4 @@
+import subprocess
+
+subprocess.call("python collect_files_dev_1.py C:\\tungphong\\pbvic_python\\pbvic_IDE_pycharm\\test_folder", shell=True)
+#subprocess.call("python collect_files_dev_1.py, shell=True)
